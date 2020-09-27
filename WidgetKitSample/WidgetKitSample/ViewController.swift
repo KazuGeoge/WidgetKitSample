0645b3a78d8a35c7537fe2420f6396c6543ed2b5
KazuGeoge <punch.rocky2@gmail.com>
2020-09-27 21:48:12 +0900

    プロジェクトをプッシュ

---
+++ WidgetKitSample/WidgetKitSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetKitSample
+//
+//  Created by 城島一輝 on 2020/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
